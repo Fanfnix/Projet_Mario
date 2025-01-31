d22d3c1df1d5588096cdd133ce1271c558806eeb
Fanfnix <valentin@familledenis.net>
2025-01-31 14:24:35 +0100

    31/01 14:24 : Modification du drapeau

--- design/test_design.c
+++ design/test_design.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// DRAPEAU : drapeau, 5, 3, 8
+// DRAPEAU : drapeau, 6, 5, 11
 // GOOMBA : goomba, 2, 3, 2
 // MARIO : mario, 3, 2, 4
 // PLANTE : plante, 4, 1, 3
@@ -47,7 +47,7 @@ int main() {
     initscr();
 
     // Modifiiez les valeurs de a_teste pour tester (les valeurs a entrer sont au début du programme)
-    struct Elem a_teste = {"drapeau", 5, 3, 8};
+    struct Elem a_teste = {"drapeau", 6, 5, 11};
     affichage(a_teste.nom, a_teste.nb_frame, a_teste.width, a_teste.height);
 
     printw("\n>>> Press any to quit :");
