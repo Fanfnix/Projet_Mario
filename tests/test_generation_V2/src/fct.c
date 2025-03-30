ebd4b56c624ac9d73b9f3b20667c067e6c1cd0d9
Valentin Denis <valentin@familledenis.net>
2025-03-30 13:24:57 +0200

    30/03 13:24 : Correction fct.c pour les windowsiens

--- tests/test_generation_V2/src/fct.c
+++ tests/test_generation_V2/src/fct.c
@@ -155,6 +155,8 @@ void libMemMap(struct Map* niv) {
 }
 
 
+#if __linux__
+
 void afficherMap_simp(WINDOW* fenetre, struct Map* niv, int height_carte, int width_carte) {
     char ch;
     for (int y = 0; y < height_carte; y++) {
@@ -172,29 +174,7 @@ void afficherMap_simp(WINDOW* fenetre, struct Map* niv, int height_carte, int wi
     }
 }
 
-void afficherMap_W(struct Map * niv, int height_carte, int width_carte) {
-    char str[TX+1];
- for (int y = 0; y < height_carte; y++) {
-     for (int i = 0; i < 2; i++) {
-         for (int x = 0; x < width_carte; x++) {
-             switch (niv->carte[y][x]) {
-                 case 0: strcpy(str, "   "); break;
-                 case 1: strcpy(str, "###"); break;
-                 case 2: strcpy(str, "OOO"); break;
-                 case 3: if (!i) strcpy(str, "==="); else strcpy(str, "=?="); break;
-                 case 4: if (!i) strcpy(str, "($)"); else strcpy(str, "   "); break;
-                 default: strcpy(str, "???"); break;
-             }
-             gotoxy(TX*x+1, TY*y+i+1);
-             putch(str[TX+1]);
-         }
-     }
- }
- return; // version windows a completer -- Normalement fait
-}
 
-
-// Affichage Map
 void afficherMap(WINDOW* fenetre, struct Map * niv, int height_carte, int width_carte) {
     char str[TX+1];
     for (int y = 0; y < height_carte; y++) {
@@ -214,24 +194,6 @@ void afficherMap(WINDOW* fenetre, struct Map * niv, int height_carte, int width_
     }
 }
 
-void afficherMap_simp_W(struct Map* niv, int height_carte, int width_carte) {
-    char ch;
-    for (int y = 0; y < height_carte; y++) {
-        for (int x = 0; x < width_carte; x++) {
-            switch (niv->carte[y][x]) {
-                case 0: ch = ' '; break;
-                case 1: ch = '#'; break;
-                case 2: ch = 'O'; break;
-                case 3: ch = '&'; break;
-                case 4: ch = '$'; break;
-                default: ch = '?'; break;
-            }
-            gotoxy(x+1, y+1);
-            putch(ch);
-        }
-    }
-    return; // version windows a completer -- Normalement fait
-}
 
 void afficherTmp(WINDOW* tmp, int X, int Y, int dMax, int* table, int seed) {
     wborder(tmp, '|', '|', '-', '-', '+', '+', '+', '+');
@@ -250,6 +212,48 @@ void afficherTmp(WINDOW* tmp, int X, int Y, int dMax, int* table, int seed) {
 	
     wrefresh(tmp);
 }
+
+#elif _WIN32
+
+void afficherMap_W(struct Map * niv, int height_carte, int width_carte) {
+    char str[TX+1];
+    for (int y = 0; y < height_carte; y++) {
+        for (int i = 0; i < 2; i++) {
+            for (int x = 0; x < width_carte; x++) {
+                switch (niv->carte[y][x]) {
+                    case 0: strcpy(str, "   "); break;
+                    case 1: strcpy(str, "###"); break;
+                    case 2: strcpy(str, "OOO"); break;
+                    case 3: if (!i) strcpy(str, "==="); else strcpy(str, "=?="); break;
+                    case 4: if (!i) strcpy(str, "($)"); else strcpy(str, "   "); break;
+                    default: strcpy(str, "???"); break;
+                }
+                gotoxy(TX*x+1, TY*y+i+1);
+                putch(str[TX+1]);
+            }
+        }
+    }
+}
+
+
+void afficherMap_simp_W(struct Map* niv, int height_carte, int width_carte) {
+    char ch;
+    for (int y = 0; y < height_carte; y++) {
+        for (int x = 0; x < width_carte; x++) {
+            switch (niv->carte[y][x]) {
+                case 0: ch = ' '; break;
+                case 1: ch = '#'; break;
+                case 2: ch = 'O'; break;
+                case 3: ch = '&'; break;
+                case 4: ch = '$'; break;
+                default: ch = '?'; break;
+            }
+            gotoxy(x+1, y+1);
+            putch(ch);
+        }
+    }
+}
+
 
 void afficherTmp_W(int X, int Y, int dMax, int* table, int seed) {
     clrscr();
@@ -271,3 +275,5 @@ void afficherTmp_W(int X, int Y, int dMax, int* table, int seed) {
     cputs(elem3);	
     return; // version windows a completer
 }
+
+#endif
