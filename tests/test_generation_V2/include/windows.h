c240dcd56f708af9d77b060a63999a038ba7755c
Fanfnix <valentin@familledenis.net>
2025-04-02 09:42:10 +0200

    correction windows.h

--- tests/test_generation_V2/include/windows.h
+++ tests/test_generation_V2/include/windows.h
@@ -1,21 +1,21 @@
 #ifndef WIN
 #define WIN
 
-typedef struct  WINDOW
-{
+#include <conio.h>
+
+typedef struct WINDOW {
     int xo;
     int yo;
     int height;
     int width;
-    struct WINDOW *precedent;    
-}WINDOW;
+    struct WINDOW *precedent;
+} WINDOW;
 
-newwin(int height, int width, int yo, int xo);
-delwin(WINDOW *win, int height, int width, int yo, int xo);
-wborder(WINDOW *win, char gauche, char droit, char haut, char bas, char haut_g, char haut_d, char bas_g, char bas_d);
-wrefresh(WINDOW *win);
-mvwaddstr(WINDOW *win, int y, int x, int str);
-mvwaddch(WINDOW *win, int y, int x, int ch);
-
+WINDOW *newwin(int height, int width, int yo, int xo);
+WINDOW *derwin(WINDOW *win, int height, int width, int yo, int xo);
+void wborder(WINDOW *win, char gauche, char droit, char haut, char bas, char haut_g, char haut_d, char bas_g, char bas_d);
+void wrefresh(WINDOW *win);
+void mvwaddstr(WINDOW *win, int y, int x, int str);
+void mvwaddch(WINDOW *win, int y, int x, int ch);
 
 #endif
