08abaf959944b0ef494059bd5ed8fb202ab151e5
Allan <allan.seene@outlook.fr>
2025-04-02 10:07:06 +0200

    pour pull ^^

--- tests/test_generation_V2/src/windows.c
+++ tests/test_generation_V2/src/windows.c
@@ -28,12 +28,11 @@ WINDOW *derwin(WINDOW *win, int height, int width, int yo, int xo) {
 }
 
 void mvwaddstr(WINDOW *win, int y, int x, char str[]){
-
-};
+}
 
 void mvwaddch(WINDOW *win, int y, int x, char ch){
 
-};
+}
 
 void wborder(WINDOW *win, char gauche, char droit, char haut, char bas, char haut_g, char haut_d, char bas_g, char bas_d){
 
