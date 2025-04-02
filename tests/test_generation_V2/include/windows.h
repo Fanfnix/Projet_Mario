36923a3e8468793302428bd566fd09142a000dc9
Allan <allan.seene@outlook.fr>
2025-04-02 09:20:39 +0200

    struct window

--- tests/test_generation_V2/include/windows.h
+++ tests/test_generation_V2/include/windows.h
@@ -1,4 +1,17 @@
 #ifndef WIN
 #define WIN
 
+#include <conio.h>
+#include <windows.h>
+
+
+typedef struct  
+{
+    int xo;
+    int yo;
+    int height;
+    int width;
+    WINDOW *precedent;    
+}WINDOW;
+
 #endif
