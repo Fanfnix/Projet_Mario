9b2b95b0fda35a33a7a050b9d98ca72d5c989800
Allan <allan.seene@outlook.fr>
2025-04-02 09:37:52 +0200

    prototype fct windows

--- tests/test_generation_V2/include/windows.h
+++ tests/test_generation_V2/include/windows.h
@@ -1,21 +1,21 @@
 #ifndef WIN
 #define WIN
 
-typedef struct  
+typedef struct  WINDOW
 {
     int xo;
     int yo;
     int height;
     int width;
-    WINDOW *precedent;    
+    struct WINDOW *precedent;    
 }WINDOW;
 
-newwin(height, width, yo, xo);
-delwin(win, height, width, yo, xo);
-wborder(win, "...");
-wrefresh();
-mvwaddstr(win, y, x, str);
-mvwaddch(win, y, x, ch);
+newwin(int height, int width, int yo, int xo);
+delwin(WINDOW *win, int height, int width, int yo, int xo);
+wborder(WINDOW *win, char gauche, char droit, char haut, char bas, char haut_g, char haut_d, char bas_g, char bas_d);
+wrefresh(WINDOW *win);
+mvwaddstr(WINDOW *win, int y, int x, int str);
+mvwaddch(WINDOW *win, int y, int x, int ch);
 
 
 #endif
---
+++ tests/test_generation_V2/src/windows.c
@@ -0,0 +1,26 @@
+#include "../include/windows.h"
+
+
+newwin(int height, int width, int yo, int xo){
+
+};
+
+delwin(WINDOW *win, int height, int width, int yo, int xo){
+
+};
+
+wborder(WINDOW *win, char gauche, char droit, char haut, char bas, char haut_g, char haut_d, char bas_g, char bas_d){
+
+};
+
+wrefresh(WINDOW *win){
+
+};
+
+mvwaddstr(WINDOW *win, int y, int x, int str){
+
+};
+
+mvwaddch(WINDOW *win, int y, int x, int ch){
+
+};
