9f49e9bd954a730f9cded67eb2b1b9d993399abb
Allan <allan.seene@outlook.fr>
2025-05-09 16:41:40 +0200

    Début d'arrivée de Mario

--- projet/include/jeu.h
+++ projet/include/jeu.h
@@ -1,8 +1,18 @@
 #ifndef JEU
 #define JEU
 
-void lancerPartie();
 
+typedef struct  Mario {
+    int id;
+    int X, Y;
+    float x, y;
+    float speed;
+    int vies
+}Mario;
+
+struct Mario * creerMario(int id, float x, float y, float speed, int vies);
+void initMario(Mario * perso, Map *niv);
 void avancerMapChunk(struct Map * niv, int * table, int * seed);
+
 
 #endif
--- projet/src/jeu.c
+++ projet/src/jeu.c
@@ -12,3 +12,23 @@ void avancerMapChunk(struct Map * niv, int * table, int * seed){
     struct Chunk * newChunk = genererChunk(niv, niv->carte [niv->nb_chunks - 2]->id, table, seed);
     ajouterChunk(niv, newChunk);
 }
+
+struct Mario * creerMario(int id, float x, float y, float speed, int vies) {
+    struct Mario * perso = malloc(sizeof(struct Mario));
+    if (perso == NULL) return NULL;
+    perso->id = id;
+    perso->X = convInt(x);
+    perso->Y = convInt(y);
+    perso->x = x;
+    perso->y = y;
+    perso->speed = speed;
+    perso->vies=vies;
+    return perso;
+}
+
+void initMario(Mario * perso, struct Map *niv){
+    perso->X = 5;
+    
+}
+niv coord x y jusqua bloc -1 cast int a float
+
