718a789b528b63dd9388c30b1d0cd7138cb7b689
Allan <allan.seene@outlook.fr>
2025-05-12 11:01:24 +0200

    5e test Mario

--- projet/src/jeu.c
+++ projet/src/jeu.c
@@ -13,24 +13,30 @@ void avancerMapChunk(struct Map * niv, int * table, int * seed){
     ajouterChunk(niv, newChunk);
 }
 
-struct Mario * creerMario(int id, float x, float y, float speed, int vies) {
+struct Mario * creerMario(float speed, int vies) {
     struct Mario * perso = malloc(sizeof(struct Mario));
     if (perso == NULL) return NULL;
-    perso->id = id;
-    perso->x = x;
-    perso->y = y;
+    perso->id = 0;
     perso->speed = speed;
-    perso->vies=vies;
+    perso->vies= 3;
     return perso;
 }
 
-void initMario(Mario * perso, struct Map * niv, WIN * fenetre, int y){
+void initMario(Mario * perso, struct Map * niv, WIN * fenetre){
     if (perso == NULL || niv == NULL || fenetre == NULL) return;
     perso->x = 5;
-    while (fenetre->fenetre[y-1] != "###")
+    int id_chunk = perso->x / DISTANCE;
+    struct Chunk * tmp_chunk = niv->p_chunk;
+    for (int i = 0; i < id_chunk; i++)
     {
-        y--;
+        tmp_chunk = tmp_chunk->suivant;
     }
-    perso->y = y;
-    perso->vies = 3;
+    for (int j = 0; j < tmp_chunk->height; j++)
+    {
+        if (tmp_chunk->area[j][convint(perso->x)] == 1 || tmp_chunk->area[j][convint(perso->x)] == 2)
+        {
+            perso->x = tmp_chunk->height - j;
+            break;
+        }     
+    }
 }
